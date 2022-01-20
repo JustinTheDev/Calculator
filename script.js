fbc10f903f13474c0455d6ab844981e09ece67fa
Justin <Justin.TheDev@gmail.com>
2022-01-20 12:31:44 -0600

    Core calc functionality added, some general checks and extended functionality completed.

--- script.js
+++ script.js
@@ -1,39 +1,83 @@
+let number1;
+let number2;
+let op;
 function add(num1, num2)
 {
-    console.log("add " + num1 + " and " + num2);
+    return(num1 + num2);
 } 
 function subtract(num1, num2)
 {
-    console.log("subtract " + num1 + " and " + num2);
+    return(num1-num2);
 } 
 function multiply(num1, num2)
 {
-    console.log("multiply " + num1 + " and " + num2);
+    return(num1*num2);
 } 
 function divide(num1, num2)
 {
-    console.log("divide " + num1 + " and " + num2);
+    return(num1/num2);
 } 
-function operate(equation)
+function operate(num1, op, num2)
 {
-    switch(equation[1])
+
+    switch(op)
     {
         case "+":
+            return (add(Number(num1), Number(num2)));
             break;
         case "-":
+            return (subtract(Number(num1), Number(num2)));
             break;
         case "/":
+            return (divide(Number(num1), Number(num2)));
             break;
         case "*":
+            return(multiply(Number(num1), Number(num2)));
             break;
-        case "^":
+        case "Xn":
             break;
     }
 }
 function getButtonDown(btn)
 {
     let displayText = document.getElementById("display");
-    if(displayText.innerText.length < 10)
+    if(btn == "C")
+    {
+        displayText.innerText = "";
+        number1 = null;
+        number2 =null;
+        op = null;
+        return;
+    }
+    else if (isNaN(btn) && btn != ".")
+    {
+        if(btn == "=" && number1 != null)
+        {
+            number2 = displayText.innerText;
+            displayText.innerText = operate(number1, op, number2);
+            number1=displayText.innerText;
+            number2=null;
+            op=null;
+            return;
+        }
+
+        if (number1 == null)
+        {
+            number1 = displayText.innerText;
+            op = btn;
+            displayText.innerText = "";
+            console.log("you pressed " + btn + " and the first number is " + number1);
+            return;
+        }
+        else if(number1 != null && number2 == null)
+        {
+            number2 = displayText.innerText;
+            op = btn;
+            displayText.innerText = "";
+        }
+        return;
+    }
+    if(displayText.innerText.length < 10 )
     {
         displayText.innerText += btn;
     }
